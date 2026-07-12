77f522051d3ba6bd0bbd2d2f2732184d0c58dcc9
swaifxcorp-cmyk <swaifxcorp@gmail.com>
2026-07-13 01:51:31 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open('v1').then(cache => cache.addAll(['/index.html', '/admin.html', '/academy.html', '/news.html', '/subscription.html'])));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
